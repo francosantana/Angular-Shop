e928560a14e04d7ea3eda1c7188233e1c7b3703d
francosantana <francosantanaus@gmail.com>
2023-07-22 13:19:37 -0400

    Navbar creation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from './auth/service/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 
+// State managment
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { authReducer } from './auth/state/auth-reducer';
 import { AuthEffects } from './auth/state/auth-effects';
+
+// App Components/Services
 import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './auth/service/auth.service';
+
+// Styles Components (MUI)
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -20,7 +28,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     StoreModule.forRoot({auth: authReducer}),
     StoreDevtoolsModule.instrument(),
     EffectsModule.forRoot([AuthEffects]),
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   exports: [
     HttpClientModule,
