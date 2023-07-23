b01c9cbb7ff00a6e5ba7279f7fa7c65ea29df1ff
francosantana <francosantanaus@gmail.com>
2023-07-23 17:55:03 -0400

    Navbar sidenav menu

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { SidenavService } from '../sidenav/service/sidenav.service';
+
+import { Store, select } from '@ngrx/store';
+import { RootState } from '../auth/models/auth-model';
+import { selectUser } from '../auth/state/auth-selector';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  user$ = this.state.pipe(select(selectUser))
+
+  constructor(private state: Store<RootState>, public sidenav: SidenavService){}
 
 }
