d24a88502ba76e7b0f31c122f3a60e272a1111fe
francosantana <francosantanaus@gmail.com>
2023-07-23 14:54:31 -0400

    Login component view

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  exports: [MatButtonModule, ReactiveFormsModule, CommonModule]
+  exports: [
+    MatButtonModule, 
+    ReactiveFormsModule, 
+    CommonModule, 
+    MatFormFieldModule,
+    MatInputModule]
 })
 export class SharedModule { }
