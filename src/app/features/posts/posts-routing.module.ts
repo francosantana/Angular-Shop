f794f4c74d0694f4aa04bfdd93a268e9c1ec6f62
francosantana <francosantanaus@gmail.com>
2023-07-24 14:42:41 -0400

    Post-details component structure and rotue

--- src/app/features/posts/posts-routing.module.ts
+++ src/app/features/posts/posts-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllPostsComponent } from './components/allposts/allposts.component';
 import { SearchComponent } from './components/search/search.component';
-import { PostComponent } from './components/post/post.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
 const routes: Routes = [
     {path: '', component: AllPostsComponent },
     {path: 'search', component: SearchComponent },
-    {path: ':id', component: PostComponent },
+    {path: ':id', component: PostDetailComponent },
     
 ];
 
--- src/app/features/posts/posts.module.ts
+++ src/app/features/posts/posts.module.ts
@@ -5,13 +5,15 @@ import { SearchComponent } from './components/search/search.component';
 import { PostComponent } from './components/post/post.component';
 import { PostsRoutingModule } from './posts-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
 
 @NgModule({
   declarations: [
     AllPostsComponent,
     SearchComponent,
-    PostComponent
+    PostComponent,
+    PostDetailComponent
   ],
   imports: [
     CommonModule,
