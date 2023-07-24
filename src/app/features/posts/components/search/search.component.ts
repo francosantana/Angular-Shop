50152efe265e8e8c0a6897439af548a57ff5ec1e
francosantana <francosantanaus@gmail.com>
2023-07-24 10:46:40 -0400

    Search state and component

--- src/app/features/posts/components/search/search.component.ts
+++ src/app/features/posts/components/search/search.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { PostsService } from '../../service/posts.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+  searchInput = new FormControl('')
+
+  constructor(private postsService: PostsService){}
+
+  ngOnInit(): void {
+    this.searchInput.valueChanges.subscribe(
+      value => {
+        if(!value) return
+        this.postsService.searchPost(value).subscribe()
+      }
+    )
+  }
+
+
 
 }
--- src/app/features/posts/service/posts.service.ts
+++ src/app/features/posts/service/posts.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap, distinctUntilChanged } from 'rxjs';
+
+import { SearchResponse } from '../models/posts.model';
+import { environment } from 'src/environment/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  private search$ = new BehaviorSubject<SearchResponse>({
+    posts: [],
+    total: 0,
+    skip: 0,
+    limit: 0
+  })
 
-  constructor() { }
+  //Skip all the followed empty emissions
+  searchState$ = this.search$.asObservable().pipe(distinctUntilChanged(
+    (prev, curr) =>{
+      return ((prev.total === curr.total) && curr.total === 0 )
+    }
+  ))
+
+  searchPost(key: string){
+    return this.http.get<SearchResponse>(environment.apiUrl + `/posts/search?q=${key}`).pipe(
+      tap(response => this.search$.next(response))
+    )
+  }
+
+  constructor(private http: HttpClient) { }
 }
