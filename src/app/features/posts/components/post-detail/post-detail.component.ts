cf0f5100d40e0da7398ec3e2c365b0e93431b0e8
francosantana <francosantanaus@gmail.com>
2023-07-25 10:41:11 -0400

    Post detail component and view

--- src/app/features/posts/components/post-detail/post-detail.component.ts
+++ src/app/features/posts/components/post-detail/post-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostsService } from '../../service/posts.service';
+import { Post } from '../../models/posts.model';
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss']
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private postsService: PostsService){}
+  post: Post | null = null
+
+  ngOnInit(): void {
+    const postId = Number(this.route.snapshot.paramMap.get('id'))
+
+    // Not allow empty id or NaN number
+    if(!postId || isNaN(postId)) return
+  
+    this.postsService.getPost(postId).subscribe(
+      post =>{
+        //ToDo : Handle post on component
+        this.post = post
+      }
+    )
+  }
+
 
 }
