bafb0fd1881efdfd9f0b59f5a06cbc360681ee94
francosantana <francosantanaus@gmail.com>
2023-07-25 16:07:40 -0400

    Login page, redirect to posts on login

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 // Store imports
 import { Store, select } from '@ngrx/store';
 import { RootState } from 'src/app/core/auth/models/auth-model';
 import { loginAction } from 'src/app/core/auth/state/auth-actions';
-import { selectError } from 'src/app/core/auth/state/auth-selector';
+import { selectError, selectUser } from 'src/app/core/auth/state/auth-selector';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   form = new FormGroup({
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required)
@@ -20,11 +21,17 @@ export class LoginComponent {
 
   $error = this.state.pipe(select(selectError))
 
-  constructor(private state: Store<RootState>){ }
+  constructor(private state: Store<RootState>, private router: Router){ }
+
+  ngOnInit(): void {
+    this.state.pipe(select(selectUser)).subscribe(
+      user => {
+        if(user) this.router.navigate(['/posts'])
+      }
+    )
+  }
 
   submitForm(){
-
-    console.log('submited')
     const { username, password} =  this.form.value
     if(!username || !password || this.form.invalid) return
     
